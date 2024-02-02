d59dae0748257b4d3f437d16797206fa6759cf42
Hugo <76200652+Hug0Rodriguez@users.noreply.github.com>
2024-02-02 06:59:43 -0500

    giving up

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Login from "./App/Screens/LoginScreen/Login";
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-react";
 
 export default function App() {
     return (
-        <View style={styles.container}>
-            <Login />
-            <StatusBar style="auto" />
-        </View>
+        <ClerkProvider publishableKey="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ5aXlzb21kc3R5c2ZpbmZoZWR4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY3NDkxNzcsImV4cCI6MjAyMjMyNTE3N30.Nqi1OlRIJiWeMbvURDb8JVxtLjOrFp7yLhFjvEdoiFk">
+            <View style={styles.container}>
+                {/* Sign in Component */}
+
+                <SignedIn>
+                    <Text>You are Signed in</Text>
+                </SignedIn>
+                {/* Sign out Component */}
+                <SignedOut>
+                    <Login />
+                </SignedOut>
+                <StatusBar style="auto" />
+            </View>
+        </ClerkProvider>
     );
 }
 
